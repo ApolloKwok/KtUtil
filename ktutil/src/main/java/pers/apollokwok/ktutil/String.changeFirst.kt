6b99972cb850c3219567764ad652077931d2ab8b
WilliamKwokX <1425183716@qq.com>
2023-01-31 01:14:45 +0800

    1, change the structure 2, upload APACHE_LICENSE

--- ktutil/src/main/java/pers/apollokwok/ktutil/String.changeFirst.kt
+++
@@ -1,4 +0,0 @@
-package pers.apollokwok.ktutil
-
-public fun String.lowercaseFirst(): String = replaceFirstChar { it.lowercase() }
-public fun String.uppercaseFirst(): String = replaceFirstChar { it.uppercase() }
